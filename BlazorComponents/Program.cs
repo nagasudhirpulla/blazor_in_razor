a16ed8975f86f453c12010e3f4dc64c6b8692b63
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2023-10-09 18:32:21 +0530

    bare minimum setup for blazor components in razor pages project

--- BlazorComponents/Program.cs
+++ BlazorComponents/Program.cs
@@ -1,10 +1,10 @@
-using BlazorInRazor.Client;
+using BlazorComponents;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 //builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+//builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
